04d9b89b9fbdf4f6eb192ac123189dee4d8b3947
omatt <omar.matthew@gmail.com>
2015-03-30 12:51:02 +0800

    Added javadoc on GorillaDialog.

--- app/src/main/java/com/averageguys/gorillalib/GorillaDialog.java
+++ app/src/main/java/com/averageguys/gorillalib/GorillaDialog.java
@@ -4,7 +4,7 @@
  * 						Omar Matthew Reyes
 
  * DATE CREATED:		June 4, 2014
- * DATE LAST UPDATED:	March 25, 2015
+ * DATE LAST UPDATED:	March 30, 2015
  *
  * TIME CREATED:		10:00 AM
  *
@@ -24,12 +24,29 @@ import android.widget.Toast;
  * Created by Android Dev on 25/3/2015.
  */
 public class GorillaDialog {
-    public static void showAlertDialog(Activity activity, String title, String msg, int dIcon, String buttonTitle) {
+    /**
+     * Invokes an AlertDialog popup on view.
+     *
+     * @param activity Current activity
+     * @param title Title for the AlertDialog
+     * @param msg Message content
+     * @param dIcon Image for the icon beside the title
+     * @param buttonPositiveTitle Text on the positive button
+     * @param buttonNegativeTitle Text on the negative button
+     *
+     * @see     android.app.AlertDialog
+     */
+    public static void showAlertDialog(Activity activity, String title, String msg, int dIcon,
+                                       String buttonPositiveTitle, String buttonNegativeTitle) {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
         alertDialogBuilder.setTitle(title);
         alertDialogBuilder.setMessage(msg);
         alertDialogBuilder.setIcon(dIcon);
-        alertDialogBuilder.setNegativeButton(buttonTitle, new DialogInterface.OnClickListener() {
+        alertDialogBuilder.setNegativeButton(buttonNegativeTitle, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alertDialogBuilder.setPositiveButton(buttonPositiveTitle, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
             }
         });
@@ -38,11 +55,23 @@ public class GorillaDialog {
         alert.show();
     }
 
-    public static void showAlertDialog(Activity activity, String title, String msg, String buttonTitle) {
+    /**
+     * Invokes an AlertDialog popup on view.
+     *
+     * @param activity Current activity
+     * @param title Title for the AlertDialog
+     * @param msg Message content
+     * @param dIcon Image for the icon beside the title
+     * @param buttonNegativeTitle Text on the negative button
+     *
+     * @see     android.app.AlertDialog
+     */
+    public static void showAlertDialog(Activity activity, String title, String msg, int dIcon, String buttonNegativeTitle) {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
         alertDialogBuilder.setTitle(title);
         alertDialogBuilder.setMessage(msg);
-        alertDialogBuilder.setNegativeButton(buttonTitle, new DialogInterface.OnClickListener() {
+        alertDialogBuilder.setIcon(dIcon);
+        alertDialogBuilder.setNegativeButton(buttonNegativeTitle, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
             }
         });
@@ -51,14 +80,52 @@ public class GorillaDialog {
         alert.show();
     }
 
+    /**
+     * Invokes an AlertDialog popup on view.
+     *
+     * @param activity Current activity
+     * @param title Title for the AlertDialog
+     * @param msg Message content
+     * @param buttonNegativeTitle Text on the negative button
+     *
+     * @see     android.app.AlertDialog
+     */
+    public static void showAlertDialog(Activity activity, String title, String msg, String buttonNegativeTitle) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
+        alertDialogBuilder.setTitle(title);
+        alertDialogBuilder.setMessage(msg);
+        alertDialogBuilder.setNegativeButton(buttonNegativeTitle, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+    /**
+     * Invokes a toast.
+     *
+     * @param activity Current activity
+     * @param msg Message content
+     *
+     * @see     android.widget.Toast
+     */
     public static void showToast(Activity activity, String msg) {
         Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * Invokes a ProgressDialog.
+     *
+     * @param activity Current activity
+     * @param title Title for the ProgressDialog
+     * @param msg Message content
+     *
+     * @see     android.app.ProgressDialog
+     */
     // [class].createProgressDialog(getActivity(), "Title", "Message").show;
-    public static ProgressDialog createProgressDialog(Activity activity, String title,String msg) {
-        ProgressDialog mProgressDialog = ProgressDialog.show(activity, title, msg, true);
-
-        return mProgressDialog;
+    public static ProgressDialog createProgressDialog(Activity activity, String title, String msg) {
+        return ProgressDialog.show(activity, title, msg, true);
     }
 }
